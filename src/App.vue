e39b28145c2ec9c5c376119274230aa7633722ae
Ali1213 <yusili2000@163.com>
2017-04-19 23:43:07 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
+  <div id="app" :seller="seller">
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+const ERR_OK = 0
 export default {
+  data () {
+    return {
+      seller: {}
+    }
+  },
+  created () {
+    this.$http.get('/api/seller').then((response) => {
+      response = response.body
+      if (response.errno === ERR_OK) {
+        this.seller = response.data
+      } else {
+
+      }
+    })
+  },
   name: 'app'
 }
 </script>
--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,1 +1,28 @@
-// 
+<template>
+  <div class="header">
+    <div class="content-wrapper">
+        <div class="avatar">
+            <img width="64" height="64" :src="seller.avatar" />
+        </div>
+    </div>
+    
+    <div class="bulletin-wrapper"></div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'header',
+  props: {
+    seller: {
+      type: Object
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
