0e4b0f536379ae26bd7c29030dd38d22b307fab3
Ali1213 <yusili2000@163.com>
2017-05-07 21:15:40 +0800

    feat:1.增加了购物车的样式;2.增加了购物车对计算属性的逻辑

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
         <router-link to="/seller">商家</router-link>
       </div>
     </div>
-    <router-view to="/goods"></router-view>
+    <router-view to="/goods" :seller="seller"></router-view>
   </div>
 </template>
 
