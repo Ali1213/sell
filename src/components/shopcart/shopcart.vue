533426d18c9aca3737b30d187160558f12411364
Ali1213 <yusili2000@163.com>
2017-05-14 19:49:29 +0800

    feat；1.complete shopcart style and animation

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="shopcart">
     <div class="content">
-      <div class="content-left">
+      <div class="content-left" @click="toggleList">
         <div class="logo-wrapper">
           <div class="logo" :class="{'highlight': totalCount>0}">
             <span class="icon-shopping_cart" :class="{'highlight': totalCount>0}"></span>
@@ -16,20 +16,42 @@
           {{payDesc}}
         </div>
       </div>
-      <div class="ball-container">
-        <transition name="drop" v-on:before-enter="beforeEnter"
-                    v-on:enter="enter" v-on:after-enter="afterEnter"
-                    v-for="(ball,index) in balls" :key="index">
-          <div class="ball" v-show="ball.show">
-            <div class="inner inner-hook"></div>
-          </div>
-        </transition>
-      </div>
     </div>
+    <div class="ball-container">
+      <transition name="drop" v-on:before-enter="beforeEnter"
+                  v-on:enter="enter" v-on:after-enter="afterEnter"
+                  v-for="(ball,index) in balls" :key="index">
+        <div class="ball" v-show="ball.show">
+          <div class="inner inner-hook"></div>
+        </div>
+      </transition>
+    </div>
+    <transition name="fold">
+      <div class="shopcart-list" v-show="listShow">
+        <div class="list-header">
+          <h1 class="title">购物车</h1><span class="empty" @click="empty">清空</span>
+        </div>
+        <div class="list-content" ref="listContent">
+          <ul>
+            <li class="food" v-for="food in selectFoods">
+              <span class="name">{{food.name}}</span>
+              <div class="price">
+                <span>￥{{food.price * food.count}}</span>
+              </div>
+              <div class="cartcontrol-wrapper">
+                <cartcontrol :food="food"></cartcontrol>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import BScroll from 'better-scroll';
+  import cartcontrol from 'components/cartControl/cartControl';
   export default {
     data () {
       let balls = [];
@@ -41,7 +63,7 @@
       return {
         balls,
         dropBalls: [],
-        listShow: false
+        fold: true
       };
     },
     props: {
@@ -86,6 +108,25 @@
       },
       payClass () {
         return this.totalPrice < this.minPrice ? 'not-enough' : 'enough';
+      },
+      listShow () {
+        if (!this.totalCount) {
+          this.fold = true;
+          return false;
+        }
+        let show = !this.fold;
+        if (show) {
+          this.$nextTick(() => {
+            if (!this.scroll) {
+              this.scroll = new BScroll(this.$refs.listContent, {
+                click: true
+              });
+            } else {
+              this.scroll.refresh();
+            }
+          });
+        }
+        return !this.fold;
       }
     },
     methods: {
@@ -134,14 +175,28 @@
           ball.show = false;
           el.style.display = 'none';
         }
+      },
+      toggleList () {
+        if (!this.totalCount) {
+          return;
+        }
+        this.fold = !this.fold;
+      },
+      empty () {
+        this.selectFoods.forEach((food) => {
+          food.count = 0;
+        });
       }
     },
-    created () {
+    components: {
+      cartcontrol
     }
   };
 </script>
 
 <style lang="stylus" rels="stylesheet/stylus">
+  @import '../../common/styles/mixin.styl';
+
   .shopcart
     position: fixed
     left: 0
@@ -236,7 +291,7 @@
         left: 32px
         bottom: 22px
         z-index: 200
-        &.drop-enter,&.drop-enter-active
+        &.drop-enter, &.drop-enter-active
           transition: all 0.4s cubic-bezier(0.49, -0.29, 0.75, 0.41)
           .inner
             width: 16px
@@ -244,4 +299,57 @@
             border-radius: 50%
             background: rgb(0, 160, 220)
             transition: all 0.4s linear
+
+  .shopcart-list
+    position: absolute
+    top: 0
+    left: 0
+    z-index: -1
+    width: 100%
+    transform: translate3d(0, -100%, 0)
+    &.fold-enter-active, &.fold-leave-active
+      transition: all 0.5s
+    &.fold-enter, &.fold-leave-active
+      transform: translate3d(0, 0, 0)
+    .list-header
+      height: 40px;
+      line-height: 40px
+      padding: 0 18px
+      background: #f3f5f7
+      border-bottom: 1px solid rgba(7, 17, 27, 0.1)
+      .title
+        float: left
+        font-size: 14px
+        font-weight: 700
+        color: rgb(7, 17, 27)
+      .empty
+        float: right
+        font-size: 12px
+        color: rgb(0, 160, 220)
+    .list-content
+      padding: 0 18px
+      max-height: 217px
+      overflow: hidden
+      background: #fff
+      .food
+        position: relative
+        padding: 12px 0
+        box-sizeing: border-box
+        border-1px(rgba(7, 17, 27, 0.1))
+        .name
+          line-height: 24px
+          font-size: 14px
+          color: rgb(7, 17, 27)
+        .price
+          position: absolute
+          right: 90px
+          bottom: 12px
+          line-height: 24px
+          font-size: 14px
+          font-weight: 700
+          color: rgb(240, 20, 20)
+        .cartcontrol-wrapper
+          position: absolute
+          right: 0
+          bottom: 6px
 </style>
